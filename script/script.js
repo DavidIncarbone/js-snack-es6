13f505d8ec73376f60fce7e292327f503385e3eb
DavidIncarbone <david.incarbone@gmail.com>
2024-11-12 14:02:42 +0100

    init snack 2

--- script/script.js
+++ script/script.js
@@ -40,6 +40,47 @@ for (let i = 0; i < biciclette.length; i++) {
 
 console.log(biciLeggera);
 
+/* Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+const footballTeam = [
+
+    {
+        name: "Inter",
+        pointsScored: 0,
+        sufferedFouls: 0
+
+    },
+    {
+        name: "Juventus",
+        pointsScored: 0,
+        sufferedFouls: 0
+
+    },
+    {
+        name: "Milan",
+        pointsScored: 0,
+        sufferedFouls: 0
+
+    },
+    {
+        name: "Napoli",
+        pointsScored: 0,
+        sufferedFouls: 0
+
+    },
+
+
+
+
+
+
+]
+
 
 
 
